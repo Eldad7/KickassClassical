d9d3f3fa733ef65cd6f3c2df52485e3c75e14ea3
Eldad7 <eldad.corem@gmail.com>
2017-06-21 01:31:21 +0300

    kickassclassical.js

--- app/First/includes/kickassclassical.js
+++ app/First/includes/kickassclassical.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-	$.get("https://kickass-classical.herokuapp.com/getMixtapesFiltered", function(data, status){
+	$.post("https://kickass-classical.herokuapp.com/getMixtapesFiltered",{"instruments":["Cello"]})
+		.done(function(data, status){
 		console.log(data["data"]);
         var ul = $("#mixtapes ul");
         var arr = [];
         arr = data["data"];
         for (var i in arr){
         	console.log(arr[i]);
-        	ul.append($("<li>").attr('id', arr[i]["_id"]).append("<img>").attr("src",arr[i]["coverImg"]);
+        	ul.append($("<li>").attr('id', arr[i]["_id"]).append("<img>").attr("src",arr[i]["coverImg"]));
         }
     });
 });
