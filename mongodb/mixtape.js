43b72738750bef7e07a6af9b5105ea60870b2576
gadyez <gady@payboxapp.com>
2017-06-20 20:01:12 +0300

    small fix

--- mongodb/mixtape.js
+++ mongodb/mixtape.js
@@ -4,7 +4,6 @@ var Schema = mongoose.Schema;
 
 var mixtapeSchema = new Schema({
     title: String,
-    indexNum: Number,
     coverImg: String,
     songList: [Schema.Types.ObjectId],
     creatorName: String,
