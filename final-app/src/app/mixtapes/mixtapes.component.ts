cb8eda05df65c4969e74f4a1b5fa6a142c962c45
gadyez <gady@payboxapp.com>
2017-07-22 13:41:31 +0300

    fixed api getcomposers and instruments

--- final-app/src/app/mixtapes/mixtapes.component.ts
+++ final-app/src/app/mixtapes/mixtapes.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiHandlerService } from '../api-handler.service';
+import { LocalStorageService } from 'angular-2-local-storage';
 
 @Component({
   selector: 'app-mixtapes',
   templateUrl: './mixtapes.component.html',
-  styleUrls: ['./mixtapes.component.css']
+  styleUrls: ['./mixtapes.component.css'],
+  providers: [ApiHandlerService]
 })
 export class MixtapesComponent implements OnInit {
 
-  constructor() { }
+  private currentMixtapes: any[];
+  private loading: boolean;
+  constructor(private apiService: ApiHandlerService, private localStorage: LocalStorageService) { }
 
   ngOnInit() {
+    var t = this;
+    this.loading = true;
+    var id = '594b6596865f6a0011ebab7f';
+    // var id = this.localStorage.get(key);
+    this.apiService.apiCallGet('getUserById/'+id,function(data){
+      console.log(data);
+      if (data.status == 200) {
+        var res = JSON.parse(data._body);
+        var sendData = {
+          instruments : res.data.favInstruments
+        };
+        t.apiService.apiCallPost('getMixtapesFiltered',sendData,function(data){
+          console.log(data);
+          if (data.status == 200) {
+            var resb = JSON.parse(data._body);
+            t.currentMixtapes = resb.data;
+            t.loading=false;
+          } else {
+            console.log("error getmixtapefilterd")
+          }
+        });
+      } else {
+        console.log("error getuserbyid")
+      }
+    });
   }
-
 }
